f35a9818808d52b9de280668ade0b2a7a5e3fbf2
zloibala <boronova25@gmail.com>
2023-05-23 19:58:11 -0500

    //

---
+++ src/components/studentsList/studentData.js
@@ -0,0 +1,42 @@
+export const students = [
+  {
+    name: "Tom",
+    hobby: "soccer",
+    picture: "https://source.unsplash.com/random/?portraits&1",
+  },
+  {
+    name: "Elnura",
+    hobby: "soccer",
+    picture: "https://source.unsplash.com/random/?portraits&2",
+  },
+  {
+    name: "Aidina",
+    hobby: "bootcamps",
+    picture: "https://source.unsplash.com/random?portraits&3",
+  },
+  {
+    name: "Shahzod",
+    hobby: "travel",
+    picture: "https://source.unsplash.com/random?portraits&4",
+  },
+  {
+    name: "Murat",
+    hobby: "chess",
+    picture: "https://source.unsplash.com/random/?portraits&5",
+  },
+  {
+    name: "Esma",
+    hobby: "yoga",
+    picture: "https://source.unsplash.com/random?portraits&6",
+  },
+  {
+    name: "Alina",
+    hobby: "bicycle",
+    picture: "https://source.unsplash.com/?portraits&7",
+  },
+  {
+    name: "Tink",
+    hobby: "soccer",
+    picture: "https://source.unsplash.com/random/?portraits&8",
+  },
+];

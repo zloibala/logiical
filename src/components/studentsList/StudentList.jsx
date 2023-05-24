4c1f3e83748aec721eb5ba67e57148b6dea9ca69
zloibala <boronova25@gmail.com>
2023-05-23 19:33:22 -0500

    ....

--- src/components/studentsList/StudentList.jsx
+++ src/components/studentsList/StudentList.jsx
@@ -3,27 +3,32 @@ import "./studentList.style.css";
 import React from "react";
 
 const students = [
-  "Tom",
-  "Elnura",
-  "Aidina",
-  "Shahzod",
-  "Murat",
-  "Esma",
-  "Alina",
-  "Tink",
+  { name: "Tom", hobby: "soccer" },
+  { name: "Elnura", hobby: "soccer" },
+  { name: "Aidina", hobby: "bootcamps" },
+  { name: "Shahzod", hobby: "travel" },
+  { name: "Murat", hobby: "chess" },
+  { name: "Esma", hobby: "yoga" },
+  { name: "Alina", hobby: "bicycle" },
+  { name: "Tink", hobby: "soccer" },
 ];
 
 const StudentList = () => {
   const showStudents = students.map((el, index) => {
-    return <p>{el}</p>;
+    return (
+      <li key={index}>
+        {el.name}, likes {el.hobby}
+      </li>
+    );
   });
+
+  // with map we should always pass a key to a high parent- key={index}
+  // key will not be shown in the browser, this is for REACT
 
   return (
     <div>
-      <h1> Student List </h1>
-      <input />
+      <ol> {showStudents} </ol>
     </div>
   );
 };
-
 export default StudentList;
